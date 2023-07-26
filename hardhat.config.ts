1a2c32a986402249e676214dc907c3bc2fe89023
Thomas Haller <thomashaller@gmx.at>
2023-07-26 12:21:23 +0200

    hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,17 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  paths: {
+    artifacts: "./artifacts",
+    cache: "./cache",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  typechain: {
+    outDir: "api/src/types",
+    target: "ethers-v5",
+  },
+  
 };
 
 export default config;
