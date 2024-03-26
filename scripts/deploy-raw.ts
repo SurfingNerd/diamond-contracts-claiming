4d4c694d9ab5a121aed7656578c49d790c2a8ba1
Thomas Haller <thomashaller@gmx.at>
2024-03-26 14:27:20 +0100

    usage of contract verification in  deploy test script.

--- scripts/deploy-raw.ts
+++ scripts/deploy-raw.ts
@@ -3,6 +3,7 @@ import "@nomiclabs/hardhat-ethers";
 
 import { ClaimContract } from "../typechain-types/ClaimContract";
 
+const hre = require("hardhat");
 
 async function main() {
 
@@ -19,10 +20,31 @@ async function main() {
     const dillute2 = now + 6 * month;
     const dillute3 = now + 5 * 12 * month;
 
+    const prefix = "0x";
+    
     const contractFactory = await ethers.getContractFactory("ClaimContract");
-    const claimContract: ClaimContract = (await contractFactory.deploy(claimBeneficorAddress, beneficorDAOAddress, "0x", dillute1, dillute2, dillute3)) as ClaimContract;
+    const claimContract: ClaimContract = (await contractFactory.deploy(claimBeneficorAddress, beneficorDAOAddress, prefix, dillute1, dillute2, dillute3)) as ClaimContract;
 
     console.log('claim contract deployed to:', claimContract.address);
+
+    console.log('claim contract deployed to:', claimContract.address);
+    
+    console.log(`trying to verify.`);
+    console.log(`npx command to verify localy:`); 
+    console.log(`npx hardhat verify --network alpha2 ${claimContract.address} ${claimBeneficorAddress} ${beneficorDAOAddress} ${prefix} ${dillute1} ${dillute2} ${dillute3}` );
+
+
+    await hre.run("verify:verify", {
+        address: claimContract.address,
+        constructorArguments: [
+            claimBeneficorAddress,
+            beneficorDAOAddress,
+            prefix,
+            dillute1,
+            dillute2,
+            dillute3,
+        ],
+      });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
